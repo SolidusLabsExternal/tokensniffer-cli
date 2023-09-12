4fe640149f0d4a084114105f3d9f727540ab55e1
yaniv.shahar <yaniv.shahar@soliduslabs.com>
2023-09-12 15:08:17 +0300

    change click optional argument to a boolean flag

--- tokensniffer/cli.py
+++ tokensniffer/cli.py
@@ -26,20 +26,24 @@ def get_usage(api):
 @cli.command()
 @click.option("--chain-id", required=True, type=int)
 @click.option("--address", required=True, type=str)
-@click.option('--include-metrics', required=False, type=bool)
-@click.option('--include-tests', required=False, type=bool)
-@click.option('--block-until-ready', required=False, type=bool)
+@click.option("--include-metrics", is_flag=True)
+@click.option("--include-tests", is_flag=True)
+@click.option("--block-until-ready", is_flag=True)
 @click.pass_obj
-def get_token(api, chain_id, address, include_metrics, include_tests, block_until_ready):
-    result = api.get_token(chain_id, address, include_metrics, include_tests, block_until_ready)
+def get_token(
+    api, chain_id, address, include_metrics, include_tests, block_until_ready
+):
+    result = api.get_token(
+        chain_id, address, include_metrics, include_tests, block_until_ready
+    )
     click_echo_json(result)
 
 
 @cli.command()
 @click.option("--chain-id", required=False)
-@click.option('--limit', required=False)
-@click.option('--offset', required=False)
-@click.option('--deployer-address', required=False)
+@click.option("--limit", required=False)
+@click.option("--offset", required=False)
+@click.option("--deployer-address", required=False)
 @click.pass_obj
 def list_scam_tokens(api, chain_id, limit, offset, deployer_address):
     result = api.list_scam_tokens(chain_id, limit, offset, deployer_address)
@@ -56,8 +60,8 @@ def get_address(api, address):
 
 @cli.command()
 @click.option("--chain_id", required=False)
-@click.option('--limit', required=False)
-@click.option('--offset', required=False)
+@click.option("--limit", required=False)
+@click.option("--offset", required=False)
 @click.pass_obj
 def list_scam_addresses(api, chain_id, limit, offset):
     result = api.list_scam_addresses(chain_id, limit, offset)
